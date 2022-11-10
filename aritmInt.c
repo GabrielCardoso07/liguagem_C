2cc6c750cdbcd27b2469ecf6b4506e7fdb2a7f3f
GabrielCardoso07 <115152421+GabrielCardoso07@users.noreply.github.com>
2022-11-10 10:45:31 -0300

    Add files via upload

---
+++ aritmInt.c
@@ -0,0 +1,20 @@
+/*Arquivo: aritmInt.c
+Data criacao:06/11/2022
+Autor: Gabriel Rodrigues F Cardoso
+*/
+#include <stdio.h>
+
+int main(int argc, char** argv){
+    int X, Y, soma, sub, mult, div, mod;
+    printf("Digite dois numeros inteiros: ");
+    scanf("%d %d", &X, &Y);
+    soma = X + Y;
+    sub = X - Y;
+    mult = X * Y;
+    div = X / Y;
+    mod = X % Y;
+    printf("%d %d %d %d %d\n", soma, sub, mult, div, mod);
+
+
+    return 0;
+}
