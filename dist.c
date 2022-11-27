d179f7d80d0c20f2434332ef03b56a654c922815
GabrielCardoso07 <115152421+GabrielCardoso07@users.noreply.github.com>
2022-11-27 17:56:17 -0300

    Add files via upload

---
+++ dist.c
@@ -0,0 +1,20 @@
+/*Arquivo: dist.c
+Data criacao: 24/11/2022
+Autor: Gabriel Rodrigues Ferreira Cardoso
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float xa, xb, ya, yb, raiz;
+    printf("Digite duas coordenadas: ");
+    scanf("%f %f", &xa,  &ya);
+    printf("Digite dois pontos: ");
+    scanf("%f %f", &xb, &yb);
+
+    raiz = (sqrt(pow(xa - xb, 2) + (pow(ya - yb, 2))));
+    printf("Distancia euclidiana: %.2f\n", raiz);
+
+    return 0;
+}
