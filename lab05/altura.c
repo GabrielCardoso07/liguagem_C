009aa53fc0f655f7a2212749982ba73db74a36f8
GabrielCardoso07 <115152421+GabrielCardoso07@users.noreply.github.com>
2023-01-09 20:08:09 -0300

    Add files via upload

---
+++ lab05/altura.c
@@ -0,0 +1,31 @@
+/*Arquivo: altura.c
+Data criacao: 09/01/2023
+Autor: Gabriel Rodrigues Ferreira Cardoso
+*/
+
+#include <stdio.h>
+
+typedef
+    struct Comprimento {
+        int metros;
+        char centimetros[2];
+    }
+Comprimento;
+
+int cm(int x);
+
+int main(void) {
+    Comprimento altura; int a;
+
+    printf("Entre com uma altura: ");
+    scanf("%d.%c%c", &altura.metros, &altura.centimetros[0], &altura.centimetros[1]);
+
+    printf("Altura %dm%c%c\n", altura.metros, altura.centimetros[0], altura.centimetros[1]);
+    return 0;
+}
+
+int cm(int x) {
+    if(x < 10) return x*10;
+    if(x > 99) return cm(x/10);
+    return x;
+}
